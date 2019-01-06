60ed3310c950b4a6ab6cbb982336a01d46a726d3
triplewoodsaid <yangsenmax@foxmail.com>
2019-01-06 19:05:25 +0800

    popover

---
+++ main.js
@@ -0,0 +1,11 @@
+$(clickMe).on('click',function(){
+    $(popover).show()
+    setTimeout(() => {
+        $(document).one('click',function(){
+            $(popover).hide()
+        })
+    }, 0);
+})
+$(wrapper).on('click',function(e){
+    e.stopPropagation();
+})
