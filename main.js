e6b5c052cfd3e1d06d357ba9d5dfc1f38cfa4a4e
triplewoodsaid <yangsenmax@foxmail.com>
2019-02-16 16:03:05 +0800

    添加了‘点击按钮浮层消失‘功能

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-$(clickMe).on('click',function(){
-    $(popover).show()
-    setTimeout(() => {
-        $(document).one('click',function(){
-            $(popover).hide()
-        })
-    }, 0);
-})
-$(wrapper).on('click',function(e){
+// $(clickMe).on('click',function(){
+//     $(popover).show()
+//     setTimeout(() => {
+//         $(document).one('click',function(){
+//             $(popover).hide()
+//         })
+//     }, 0);
+// })
+// $(wrapper).on('click',function(e){
+//     e.stopPropagation();
+// }) //这种方法，再次点击wrapper时没实现popover.hide()
+
+$(wrapper).click(function(e){
     e.stopPropagation();
+    $(popover).show();
+})
+$(clickMe).on('click', (e)=>{
+    e.stopPropagation();
+    $(popover).toggle();
+})
+$(document).click(function(){
+    $(popover).hide();
 })
